d7f5c36c277ca123c26fcd42727758ccee81d029
MarcusMathuram46 <marcus.mathuram7@gmail.com>
2024-10-30 04:54:45 +0530

    Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios"; // Import axios
-import "../styles/home.css";
+import "../styles/Home.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import { Link } from "react-router-dom";
